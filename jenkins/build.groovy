2629288e525f2de2ec3d6a0f2753c5374cd0d6d6
17839307 <Sgkys$?49500>
2022-04-04 13:39:52 +0300

    jwnkins

---
+++ jenkins/build.groovy
@@ -0,0 +1,23 @@
+properties([disableConcurrentBuilds()])
+
+pipeline {
+    agent {
+        label 'master'
+        }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        timestamps()
+    }
+    stages {
+        stage("First step") {
+            steps {
+
+            }
+        }
+        stage("Second step") {
+            steps {
+
+            }
+        }
+    }
+}
